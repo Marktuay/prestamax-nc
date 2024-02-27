7cd2bd8f94daed7725dab99ee7fb6e9e32be0ea2
Marktuay <marktuay@gmail.com>
2024-02-27 11:40:08 -0600

    cambios ultimos

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-//Fecha
+/************Fecha en el sitio*****************/
 const fechaActual = new Date();
 const fechaFormateada = fechaActual.toLocaleDateString("es-ES");
-document.getElementById("fecha-actual").innerHTML = fechaFormateada;
-
-
+document.getElementById("fecha-actual").innerHTML = fechaFormateada;//linea para ponerlo en el html
+ console.log(fechaActual)
 
 
 const year = new Date().getFullYear();
         const yearElement = document.querySelector(".year");
         yearElement.textContent = year;
 
+console.log(year);
+/****************END fechas *******************/
 
