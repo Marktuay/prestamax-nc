e625d689e0afd756ecbb355aa42cbb92815f6776
Marktuay <marktuay@gmail.com>
2024-03-05 16:57:18 -0600

    Actualizaciones de colores corporativos

---
+++ assets/js/calculof.js
@@ -0,0 +1,12 @@
+// Aplicacion realizada en Javascript para realizar calculo financieron de prestamo.
+
+let  principal = '';
+let interes = '';
+let cuotas = '';
+
+// función de calculo de prestamos segun las variables y la formula del prestamo
+
+function calculo(params) {
+    
+}
+
