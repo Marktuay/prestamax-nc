0fd65c32aac7d2bbc89b88e7315970fcc1292e95
Marktuay <marktuay@gmail.com>
2024-03-27 14:04:02 -0600

    Actualizacion archivo calculof.js finalizado

--- assets/js/calculof.js
+++ assets/js/calculof.js
@@ -1,38 +1,33 @@
 
 
-// Ejemplo de uso
-//let montoPrestamo = 10000; // Ejemplo de monto de préstamo
-const montoPrestamo = document.querySelector("#monto").value;
-const tiempoMeses = document.querySelector("#tiempo").value
-const tasaInteres = 0.25; // Tasa de interés mensual (en porcentaje)
-const botonCuotaMes = document.getElementById("botonCuotaMes");
+document.addEventListener("DOMContentLoaded", function() {
+    const botonCuotaMes = document.getElementById("botonCuotaMes");
 
-//let tiempoMeses = 12; // Duración del préstamo en meses
+    botonCuotaMes.onclick = function() {
+        const montoPrestamo = parseFloat(document.querySelector("#monto").value);
+        const tiempoMeses = parseInt(document.querySelector("#tiempo").value);
+        const tasaInteres = 0.02; // Tasa de interés mensual (en porcentaje)
 
-// Cálculo de la cuota mensual incluyendo intereses
-let cuotaMensual = (montoPrestamo * tasaInteres) / (1 - Math.pow(1 + tasaInteres, -tiempoMeses));
-console.log("La cuota mensual del préstamo es: $" + cuotaMensual.toFixed(2));
+        // Cálculo de la cuota mensual incluyendo intereses
+        const cuotaMensual = (montoPrestamo * tasaInteres) / (1 - Math.pow(1 + tasaInteres, -tiempoMeses));
+        console.log("La cuota mensual del préstamo es: $" + cuotaMensual.toFixed(2));
+        let cuota = cuotaMensual;
+        document.getElementById('cuotas').innerHTML += '<tr><td>'+"$"+cuota.toFixed(2)+'</td></tr> ';
 
-
-
-// Función para calcular las cuotas mensuales con intereses
-function cuotasMes() {
-    for (let i = 1; i <= tiempoMeses; i++) {
-        let saldoRestante = montoPrestamo * Math.pow(1 + tasaInteres, i) - (cuotaMensual * (Math.pow(1 + tasaInteres, i) - 1) / tasaInteres);
-        console.log(`La cuota ${i} del préstamo es: $${cuotaMensual.toFixed(2)}, Saldo Restante: $${saldoRestante.toFixed(2)}`);
-    }
-}
-
-
-botonCuotaMes.onclick = cuotasMes; // Llamada a la función para calcular e imprimir las cuotas mensuales
-
-
-console.log(montoPrestamo);
-console.log(tiempoMeses);
+        // Función para calcular las cuotas mensuales con intereses 
+        for (let i = 1; i <= tiempoMeses; i++) {
+            const saldoRestante = montoPrestamo * Math.pow(1 + tasaInteres, i) - (cuotaMensual * (Math.pow(1 + tasaInteres, i) - 1) / tasaInteres);
+            console.log(`La cuota ${i} del préstamo es: $${cuotaMensual.toFixed(2)}, Saldo Restante: $${saldoRestante.toFixed(2)}`);
+           // if (i === tiempoMeses) {
+            //    console.log (`la ultima cuota es" ${cuotaMensual - saldoRestante}` )
+           // }
+        }
+    };
+});
 
 
- 
-/*** Pruebas Unitarias */
+
+
 
 
 
