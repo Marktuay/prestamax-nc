255b3b967134746e97883b633498ccb9f8240ad5
Marcelo Martinez <marktuay@gmail.com>
2025-01-09 09:35:40 -0600

    Update090125calculof.js
    Cambios en la tasa de interés que es el 12% y limitamos el tiempo de préstamo a 60 meses como máximo.

--- assets/js/calculof.js
+++ assets/js/calculof.js
@@ -4,31 +4,30 @@ document.addEventListener("DOMContentLoaded", function() {
     const botonCuotaMes = document.getElementById("botonCuotaMes");
 
     botonCuotaMes.onclick = function() {
-        const montoPrestamo = parseFloat(document.querySelector("#monto").value); //dato obtenido del campo del monto;
-        const tiempoMeses = parseInt(document.querySelector("#tiempo").value); //dato obteido del campo de los meses del prestamo; 
-        const tasaInteres = 0.02; // Tasa de interés mensual (en porcentaje)
+        const montoPrestamo = parseFloat(document.querySelector("#monto").value);
+        let tiempoMeses = parseInt(document.querySelector("#tiempo").value);
+        // Limitar el tiempo a un máximo de 60 meses
+        tiempoMeses = Math.min(tiempoMeses, 60);
+        
+        const tasaInteresAnual = 0.12; // Tasa de interés anual fija
+        const tasaInteresMensual = tasaInteresAnual / 12; // Convertir a tasa mensual
 
         // Limpiar las cuotas previamente calculadas
         document.getElementById('cuotas').innerHTML = '';
 
         // Cálculo de la cuota mensual incluyendo intereses
-        const cuotaMensual = (montoPrestamo * tasaInteres) / (1 - Math.pow(1 + tasaInteres, -tiempoMeses));
+        const cuotaMensual = (montoPrestamo * tasaInteresMensual) / (1 - Math.pow(1 + tasaInteresMensual, -tiempoMeses));
         console.log("La cuota mensual del préstamo es: $" + cuotaMensual.toFixed(2));
         let cuota = cuotaMensual;
         document.getElementById('cuotas').innerHTML += '<tr><td>'+"$"+cuota.toFixed(2)+'</td></tr> ';
         
-
         // Función para calcular las cuotas mensuales con intereses 
         for (let i = 1; i <= tiempoMeses; i++) {
-            const saldoRestante = montoPrestamo * Math.pow(1 + tasaInteres, i) - (cuotaMensual * (Math.pow(1 + tasaInteres, i) - 1) / tasaInteres);
+            const saldoRestante = montoPrestamo * Math.pow(1 + tasaInteresMensual, i) - (cuotaMensual * (Math.pow(1 + tasaInteresMensual, i) - 1) / tasaInteresMensual);
             console.log(`La cuota ${i} del préstamo es: $${cuotaMensual.toFixed(2)}, Saldo Restante: $${saldoRestante.toFixed(2)}`);
-           
         }
     };
 });
-
-let sum = (a, b) => a+ b;
-console.log(sum(5,7));
 
 
 
